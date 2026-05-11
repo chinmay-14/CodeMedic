aed884608f9f94d7058490fc464ead5df9a24c9a
Chinmay Sharma <chinmaysharma140806@gmail.com>
2026-05-11 17:49:19 +0530

    fixed cors for vercel

--- codemedic-backend/src/server.js
+++ codemedic-backend/src/server.js
@@ -18,7 +18,14 @@ const app = express();
 
 /* MIDDLEWARE */
 
-app.use(cors());
+app.use(cors({
+  origin:[
+    "http://localhost:5173",
+    "https://codemedic-frontend.vercel.app",
+  ],
+  credentials: true,
+})
+);
 
 app.use(express.json());
 
