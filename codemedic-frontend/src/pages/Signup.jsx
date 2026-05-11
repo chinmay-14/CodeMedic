f74ff335bc3a24648094afed56f2f36cfcbde7f9
Chinmay Sharma <chinmaysharma140806@gmail.com>
2026-05-11 17:34:29 +0530

    fixed deployed signup api

--- codemedic-frontend/src/pages/Signup.jsx
+++ codemedic-frontend/src/pages/Signup.jsx
@@ -48,7 +48,7 @@ function Signup() {
 
       const response = await axios.post(
 
-        "http://localhost:5000/api/auth/signup",
+        '${import.meta.env.VITE_BACKEND_URL}/api/auth/signup',
 
         formData
 
